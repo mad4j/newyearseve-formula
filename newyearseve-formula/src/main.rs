a3e155faf5d4970bf90c957c4110c6f41e3cad6d
Daniele Olmisani <mad4j@users.noreply.github.com>
2020-12-31 07:37:20 +0100

    update

--- newyearseve-formula/src/main.rs
+++ newyearseve-formula/src/main.rs
@@ -13,6 +13,12 @@ const FORMULA_SIZE: usize = FORMULA_NUM_OPERANDS + FORMULA_NUM_OPERATORS;
 
 const FORMULA_OPERATORS: [char; 5] = ['+', '-', '*', '/', '^'];
 
+struct Formula {
+    positions: [u8; FORMULA_NUM_OPERATORS],
+    operators: [char; FORMULA_NUM_OPERATORS],
+}
+
+
 fn generate_formula(positions: &[usize], operations: &[&char]) -> String {
     let mut o = 0;
     let mut d = 9;
@@ -32,6 +38,46 @@ fn generate_formula(positions: &[usize], operations: &[&char]) -> String {
     buf
 }
 
+fn evaluate_raw_formula(positions: &[usize], operations: &[&char]) -> Option<u32> {
+    let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS);
+
+    let mut o = 0;
+    let mut d = 9;
+
+    for i in 0..FORMULA_SIZE {
+        if i < positions[o] {
+            stack.push(d);
+            d -= 1;
+        } else {
+            let op2 = stack.pop()?;
+            let op1 = stack.pop()?;
+            let result = match operations[o] {
+                '+' => Some(op1 + op2),
+                '-' => Some(op1 - op2),
+                '*' => Some(op1 * op2),
+                '/' => {
+                    if op2 != 0 && op1 % op2 == 0 {
+                        Some(op1 / op2)
+                    } else {
+                        None
+                    }
+                }
+                '^' => Some(op1.pow(op2)),
+                _ => None,
+            };
+
+            match result {
+                Some(v) => stack.push(v),
+                None => return None,
+            }
+            o += 1;
+        }
+    }
+
+    stack.pop()
+}
+
+#[cfg(readable)]
 fn evaluate_formula(expr: &str) -> Option<u32> {
     let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS);
 
@@ -75,6 +121,7 @@ fn main() {
     // operands
 
     let start = Instant::now();
+    let mut count = 0;
 
     // generate all valid operator's positions
     let pos = (0..FORMULA_SIZE)
@@ -85,13 +132,22 @@ fn main() {
     let ops = FORMULA_OPERATORS.iter().dispositions(FORMULA_NUM_OPERATORS);
 
     for (p, o) in pos.cartesian_product(ops) {
-        let s = generate_formula(&p[..], &o[..]);
-        let r = evaluate_formula(&s).unwrap_or_default();
+        //let s = generate_formula(&p[..], &o[..]);
+        //let r = evaluate_formula(&s).unwrap_or_default();
+
+        let r = evaluate_raw_formula(&p[..], &o[..]).unwrap_or_default();
 
         if r == 2021 {
-            println!("{:?} -> {:?}", s, r);
+            println!("{:?} -> {:?}", generate_formula(&p[..], &o[..]), r);
         }
+
+        count += 1;
     }
 
-    println!("Time elapsed is {:?}", start.elapsed());
+    println!(
+        "{} iterations in {:?} @{}its",
+        count,
+        start.elapsed(),
+        count / start.elapsed().as_secs()
+    );
 }
