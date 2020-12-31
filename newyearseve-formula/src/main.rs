4119c9e7e3d4a8d9011684806c31add14384eeb6
Daniele Olmisani <mad4j@users.noreply.github.com>
2020-12-31 10:52:21 +0100

    Update main.rs

--- newyearseve-formula/src/main.rs
+++ newyearseve-formula/src/main.rs
@@ -7,30 +7,35 @@ use itertools::Itertools;
 use std::char;
 use std::time::Instant;
 
-const FORMULA_NUM_OPERANDS: usize = 9;
-const FORMULA_NUM_OPERATORS: usize = 8;
-const FORMULA_SIZE: usize = FORMULA_NUM_OPERANDS + FORMULA_NUM_OPERATORS;
+const FORMULA_NUM_OPERANDS: u8 = 9;
+const FORMULA_NUM_OPERATORS: u8 = 8;
+const FORMULA_SIZE: u8 = FORMULA_NUM_OPERANDS + FORMULA_NUM_OPERATORS;
 
 const FORMULA_OPERATORS: [char; 5] = ['+', '-', '*', '/', '^'];
 
-struct Formula {
-    positions: [u8; FORMULA_NUM_OPERATORS],
-    operators: [char; FORMULA_NUM_OPERATORS],
+#[derive(Debug)]
+pub struct Formula<'a> {
+    positions: Vec<u8>,
+    operators: Vec<&'a char>,
 }
 
+fn generate_formula(f: &Formula) -> String {
+    let &Formula {
+        ref positions,
+        ref operators,
+    } = f;
 
-fn generate_formula(positions: &[usize], operations: &[&char]) -> String {
     let mut o = 0;
     let mut d = 9;
 
-    let mut buf = String::with_capacity(FORMULA_SIZE);
+    let mut buf = String::with_capacity(FORMULA_SIZE as usize);
 
     for i in 0..FORMULA_SIZE {
         if i < positions[o] {
             buf.push(char::from_digit(d, 10).unwrap());
             d -= 1;
         } else {
-            buf.push(*operations[o]);
+            buf.push(*operators[o]);
             o += 1;
         }
     }
@@ -38,8 +43,13 @@ fn generate_formula(positions: &[usize], operations: &[&char]) -> String {
     buf
 }
 
-fn evaluate_raw_formula(positions: &[usize], operations: &[&char]) -> Option<u32> {
-    let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS);
+fn evaluate_raw_formula(f: &Formula) -> Option<u32> {
+    let &Formula {
+        ref positions,
+        ref operators,
+    } = f;
+
+    let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS as usize);
 
     let mut o = 0;
     let mut d = 9;
@@ -51,7 +61,7 @@ fn evaluate_raw_formula(positions: &[usize], operations: &[&char]) -> Option<u32
         } else {
             let op2 = stack.pop()?;
             let op1 = stack.pop()?;
-            let result = match operations[o] {
+            let result = match operators[o] {
                 '+' => Some(op1 + op2),
                 '-' => Some(op1 - op2),
                 '*' => Some(op1 * op2),
@@ -74,44 +84,11 @@ fn evaluate_raw_formula(positions: &[usize], operations: &[&char]) -> Option<u32
         }
     }
 
-    stack.pop()
-}
-
-#[cfg(readable)]
-fn evaluate_formula(expr: &str) -> Option<u32> {
-    let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS);
-
-    for token in expr.chars() {
-        let result = if token.is_digit(10) {
-            // convert char to digit and then to float
-            token.to_digit(10)
-        } else {
-            let op2 = stack.pop()?;
-            let op1 = stack.pop()?;
-
-            match token {
-                '+' => Some(op1 + op2),
-                '-' => Some(op1 - op2),
-                '*' => Some(op1 * op2),
-                '/' => {
-                    if op2 != 0 && op1 % op2 == 0 {
-                        Some(op1 / op2)
-                    } else {
-                        None
-                    }
-                }
-                '^' => Some(op1.pow(op2)),
-                _ => None,
-            }
-        };
-
-        match result {
-            Some(v) => stack.push(v),
-            None => return None,
-        };
+    if stack.len() == 1 {
+        stack.pop()
+    } else {
+        None
     }
-
-    stack.pop()
 }
 
 fn main() {
@@ -125,29 +102,33 @@ fn main() {
 
     // generate all valid operator's positions
     let pos = (0..FORMULA_SIZE)
-        .combinations(FORMULA_NUM_OPERATORS)
-        .filter(|x| x.iter().enumerate().all(|(i, v)| *v >= 2 * (i + 1)));
+        .combinations(FORMULA_NUM_OPERATORS as usize)
+        .filter(|x| x.iter().enumerate().all(|(i, v)| *v >= 2 * (i + 1) as u8));
 
     // generate all possible operators dispositions
-    let ops = FORMULA_OPERATORS.iter().dispositions(FORMULA_NUM_OPERATORS);
+    let ops = FORMULA_OPERATORS
+        .iter()
+        .dispositions(FORMULA_NUM_OPERATORS as usize);
 
     for (p, o) in pos.cartesian_product(ops) {
-        //let s = generate_formula(&p[..], &o[..]);
-        //let r = evaluate_formula(&s).unwrap_or_default();
+        let f = Formula {
+            positions: p,
+            operators: o,
+        };
 
-        let r = evaluate_raw_formula(&p[..], &o[..]).unwrap_or_default();
+        let r = evaluate_raw_formula(&f).unwrap_or_default();
 
         if r == 2021 {
-            println!("{:?} -> {:?}", generate_formula(&p[..], &o[..]), r);
+            println!("{:?} -> {:?}", generate_formula(&f), r);
         }
 
         count += 1;
     }
 
     println!(
-        "{} iterations in {:?} @{}its",
+        "{} iterations in {:?} @{}itms",
         count,
         start.elapsed(),
-        count / start.elapsed().as_secs()
+        count / start.elapsed().as_millis()
     );
 }
