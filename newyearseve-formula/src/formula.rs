db14aa5ecc6b1e48b6d597f9cbb4ed202f2d23a0
Daniele Olmisani <mad4j@users.noreply.github.com>
2020-12-31 14:50:03 +0100

    update

---
+++ newyearseve-formula/src/formula.rs
@@ -0,0 +1,97 @@
+use std::char;
+use std::fmt;
+
+pub const FORMULA_NUM_OPERANDS: u8 = 9;
+pub const FORMULA_NUM_OPERATORS: u8 = 8;
+pub const FORMULA_SIZE: u8 = FORMULA_NUM_OPERANDS + FORMULA_NUM_OPERATORS;
+
+pub const FORMULA_OPERATORS: [char; 5] = ['+', '-', '*', '/', '^'];
+
+#[derive(Debug)]
+pub struct Formula<'a> {
+    pub positions: Vec<u8>,
+    pub operators: Vec<&'a char>,
+}
+
+impl<'a> Formula<'_> {
+    pub fn new(p: Vec<u8>, o: Vec<&'a char>) -> Formula<'a> {
+        Formula {
+            positions: p,
+            operators: o,
+        }
+    }
+
+    pub fn evaluate(&self) -> Option<u32> {
+        let Formula {
+            ref positions,
+            ref operators,
+        } = self;
+
+        let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS as usize);
+
+        let mut o = 0;
+        let mut d = 9;
+
+        for i in 0..FORMULA_SIZE {
+            if i < positions[o] {
+                stack.push(d);
+                d -= 1;
+            } else {
+                let op2 = stack.pop()?;
+                let op1 = stack.pop()?;
+                let result = match operators[o] {
+                    '+' => Some(op1 + op2),
+                    '-' => Some(op1 - op2),
+                    '*' => Some(op1 * op2),
+                    '/' => {
+                        if op2 != 0 && op1 % op2 == 0 {
+                            Some(op1 / op2)
+                        } else {
+                            None
+                        }
+                    }
+                    '^' => Some(op1.pow(op2)),
+                    _ => None,
+                };
+
+                match result {
+                    Some(v) => stack.push(v),
+                    None => return None,
+                }
+                o += 1;
+            }
+        }
+
+        if stack.len() == 1 {
+            stack.pop()
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> fmt::Display for Formula<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Formula {
+            ref positions,
+            ref operators,
+        } = self;
+
+        let mut o = 0;
+        let mut d = 9;
+
+        let mut buf = String::with_capacity(FORMULA_SIZE as usize);
+
+        for i in 0..FORMULA_SIZE {
+            if i < positions[o] {
+                buf.push(char::from_digit(d, 10).unwrap());
+                d -= 1;
+            } else {
+                buf.push(*operators[o]);
+                o += 1;
+            }
+        }
+
+        write!(f, "{}", buf)
+    }
+}
--- newyearseve-formula/src/main.rs
+++ newyearseve-formula/src/main.rs
@@ -1,95 +1,12 @@
 //https://www.dcode.fr/reverse-polish-notation
 
 mod dispositions;
+mod formula;
 
 use crate::dispositions::*;
+use crate::formula::*;
 use itertools::Itertools;
-use std::char;
 use std::time::Instant;
-
-const FORMULA_NUM_OPERANDS: u8 = 9;
-const FORMULA_NUM_OPERATORS: u8 = 8;
-const FORMULA_SIZE: u8 = FORMULA_NUM_OPERANDS + FORMULA_NUM_OPERATORS;
-
-const FORMULA_OPERATORS: [char; 5] = ['+', '-', '*', '/', '^'];
-
-#[derive(Debug)]
-pub struct Formula<'a> {
-    positions: Vec<u8>,
-    operators: Vec<&'a char>,
-}
-
-fn generate_formula(f: &Formula) -> String {
-    let &Formula {
-        ref positions,
-        ref operators,
-    } = f;
-
-    let mut o = 0;
-    let mut d = 9;
-
-    let mut buf = String::with_capacity(FORMULA_SIZE as usize);
-
-    for i in 0..FORMULA_SIZE {
-        if i < positions[o] {
-            buf.push(char::from_digit(d, 10).unwrap());
-            d -= 1;
-        } else {
-            buf.push(*operators[o]);
-            o += 1;
-        }
-    }
-
-    buf
-}
-
-fn evaluate_raw_formula(f: &Formula) -> Option<u32> {
-    let &Formula {
-        ref positions,
-        ref operators,
-    } = f;
-
-    let mut stack: Vec<u32> = Vec::with_capacity(FORMULA_NUM_OPERANDS as usize);
-
-    let mut o = 0;
-    let mut d = 9;
-
-    for i in 0..FORMULA_SIZE {
-        if i < positions[o] {
-            stack.push(d);
-            d -= 1;
-        } else {
-            let op2 = stack.pop()?;
-            let op1 = stack.pop()?;
-            let result = match operators[o] {
-                '+' => Some(op1 + op2),
-                '-' => Some(op1 - op2),
-                '*' => Some(op1 * op2),
-                '/' => {
-                    if op2 != 0 && op1 % op2 == 0 {
-                        Some(op1 / op2)
-                    } else {
-                        None
-                    }
-                }
-                '^' => Some(op1.pow(op2)),
-                _ => None,
-            };
-
-            match result {
-                Some(v) => stack.push(v),
-                None => return None,
-            }
-            o += 1;
-        }
-    }
-
-    if stack.len() == 1 {
-        stack.pop()
-    } else {
-        None
-    }
-}
 
 fn main() {
     // formula string is 17 chars long (9 digits and 8 operators)
@@ -111,15 +28,11 @@ fn main() {
         .dispositions(FORMULA_NUM_OPERATORS as usize);
 
     for (p, o) in pos.cartesian_product(ops) {
-        let f = Formula {
-            positions: p,
-            operators: o,
-        };
+        let f = Formula::new(p, o);
+        let r = f.evaluate().unwrap_or_default();
 
-        let r = evaluate_raw_formula(&f).unwrap_or_default();
-
-        if r == 2021 {
-            println!("{:?} -> {:?}", generate_formula(&f), r);
+        if r == 2020 {
+            println!("{} -> {:?}", f, r);
         }
 
         count += 1;
